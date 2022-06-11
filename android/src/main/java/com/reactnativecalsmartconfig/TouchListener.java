f3941354835f2ecc903b245e726a8ab2ad352cdd
CLo <clowenhg@gmail.com>
2022-06-11 01:06:42 -0400

    adds import for touch listener

--- android/src/main/java/com/reactnativecalsmartconfig/TouchListener.java
+++ android/src/main/java/com/reactnativecalsmartconfig/TouchListener.java
@@ -1,8 +1,18 @@
 package com.reactnativecalsmartconfig;
 
+import com.espressif.iot.esptouch.EsptouchTask;
+import com.espressif.iot.esptouch.IEsptouchResult;
 import com.espressif.iot.esptouch.IEsptouchListener;
 
+import com.facebook.react.bridge.Promise;
+
 public class TouchListener implements IEsptouchListener {
+  Promise promise;
+
+  TouchListener(Promise promise) {
+    this.promise = promise;
+  }
+
   void onEsptouchResultAdded(IEsptouchResult result) {
     results.add(result);
     log.info(result);
