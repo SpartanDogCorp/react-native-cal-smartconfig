1b82ddec5667f6199fab1c7a3499e324da97f8af
CLo <clowenhg@gmail.com>
2022-06-11 00:36:28 -0400

    moves private class to separate file

--- android/src/main/java/com/reactnativecalsmartconfig/CalSmartconfigModule.java
+++ android/src/main/java/com/reactnativecalsmartconfig/CalSmartconfigModule.java
@@ -85,23 +85,9 @@ public class CalSmartconfigModule extends ReactContextBaseJavaModule {
     log.info("Provisioning SmartConfig");
 
     ArrayList<E> results = new ArrayList<IEsptouchResult>();
-    private class TouchListener implements IEsptouchListener {
-      void onEsptouchResultAdded(IEsptouchResult result) {
-        results.add(result);
-        log.info(result);
-
-        if (count = 0) {
-          promise.resolve(results);
-        }
-
-        if (results.size() == count) {
-          promise.resolve(results);
-        }
-      }
-    }
 
     EsptouchTask task = new EsptouchTask(apSsid, apBssid, apPassword, context);
-    task.setEsptouchListener(TouchListener);
+    task.setEsptouchListener(new TouchListener());
     task.executeForResults(count);
   }
 }
